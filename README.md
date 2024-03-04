a338f0828e7e825cc8bfced26891848d8747d588
riverallzero <kallzero1008@jbnu.ac.kr>
2024-03-04 23:02:39 +0900

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # lms-calendar
+전북대학교 LMS에서 원하는 특정 강의 영상이 올라오면 영상 출석 기간을 구글 달력에 자동으로 저장해주는 프로그램
+
+|![](./asset/before.png) | ![](./asset/after.png)
+---| ---|
+
+## setting
+### API
+1. https://riverallzero.tistory.com/76 참고해 Google Calendar API 설정 
+2. [test.py](https://github.com/riverallzero/lms-calendar/blob/main/test.py) 실행하며 작동 확인
+3. [test.py](https://github.com/riverallzero/lms-calendar/blob/main/test.py) 실행 시 생성된 [token.pickle](https://github.com/riverallzero/lms-calendar/blob/main/token.pickle)을 저장소에 업로드(기존 파일은 삭제)
+
+### secrets
+```Settings``` - ```Screts and variable``` - ```Actions``` -```New repository secrets```
+
+![](./asset/secrets.png)
+
+4. OAuth Client ID에서 다운받은 json 파일 내용의 값을 복사해 "CLIENT_SECRET_JSON_DATA"에 값 입력
+5. LMS에서 원하는 강의의 group_id(자료에서 아무 파일 선택시 url에서 나타남)를 복사한 후 "GROUP_ID"에 값 입력
+6. LMS 아이디(LMS_ID)와 비밀번호(LMS_PW)도 값을 입력
--- test.py
+++ test.py
@@ -1,122 +1,50 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-import pandas as pd
-import os
-
+from datetime import datetime, timedelta
+import datetime
 import pickle
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-def finding_video():
-    # 파일에서 마지막 인덱스 번호 읽기
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    last_index_file = os.path.join(BASE_DIR, 'last_index.txt')
-
-    if os.path.exists(last_index_file):
-        with open(last_index_file, 'r') as f:
-            last_index = int(f.read()) + 1
-    else:
-        last_index = 1
+CREDENTIALS_FILE = '다운로드_받은_파일경로.json'
 
 
-    group_id = 'lms_group_id'
-    lms_id = 'lms_id'
-    lms_pw = 'lms_pw.'
+creds = None
 
-    driver = webdriver.Chrome()
-    url = 'https://ieilms.jbnu.ac.kr/'
-    driver.get(url)
+if os.path.exists('token.pickle'):
+   with open('token.pickle', 'rb') as token:
+       creds = pickle.load(token)
 
-    # WebDriverWait를 사용하여 페이지가 로드될 때까지 최대 10초 기다림
-    wait = WebDriverWait(driver, 10)
-    element_id = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="id"]')))
-    element_pw = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="passwd"]')))
+if not creds or not creds.valid:
+   if creds and creds.expired and creds.refresh_token:
+       creds.refresh(Request())
+   else:
+       flow = InstalledAppFlow.from_client_secrets_file(
+           CREDENTIALS_FILE, SCOPES)
+       creds = flow.run_local_server(port=5090)
 
-    # LMS 아이디와 비밀번호를 입력
-    element_id.send_keys(lms_id)
-    element_pw.send_keys(lms_pw)
+   with open('token.pickle', 'wb') as token:
+       pickle.dump(creds, token)
 
-    # 로그인
-    loginbtn = driver.find_element(By.XPATH, '//*[@id="loginform"]/table/tbody/tr[1]/td[2]/input')
-    loginbtn.click()
-    wait.until(EC.invisibility_of_element_located((By.ID, 'loginform')))
+service = build('calendar', 'v3', credentials=creds)
 
-    # 영상 출석부 페이지로 이동
-    driver.get(f'https://ieilms.jbnu.ac.kr/attend/videoDataViewAttendListStudent.jsp?group_id={group_id}')
+d = datetime.now().date()
+tomorrow = datetime(d.year, d.month, d.day, 10)+timedelta(days=1)
+start = tomorrow.isoformat()
+end = (tomorrow + timedelta(hours=1)).isoformat()
 
-    # 비디오 개수 카운트
-    elements_count = len(driver.find_elements(By.CSS_SELECTOR, '#dataBox > table > tbody > tr'))
+event_result = service.events().insert(calendarId='primary',
+   body={
+       'summary': 'Automating calendar',
+       'start': {'dateTime': start, 'timeZone': 'Asia/Seoul'},
+       'end': {'dateTime': end, 'timeZone': 'Asia/Seoul'},
+   }
+).execute()
 
-    if elements_count >= 1:
-        index_list = []
-        title_list = []
-        date_list = []
-
-        for i in range(last_index, elements_count + 1):
-            last_index = driver.find_element(By.XPATH, f'//*[@id="dataBox"]/table/tbody/tr[{i}]/td[1]').text
-            index_list.append(str(last_index))
-            titleresult = driver.find_element(By.XPATH, f'//*[@id="dataBox"]/table/tbody/tr[{i}]/td[2]').text
-            title_list.append(str(titleresult))
-            dateresult = driver.find_element(By.XPATH, f'//*[@id="dataBox"]/table/tbody/tr[{i}]/td[3]').text
-            date_list.append(str(dateresult))
-
-        df = pd.DataFrame({'index': index_list, 'title': title_list, 'date': date_list})
-
-        if not df.empty:
-
-            last_num = df['index'].iloc[-1]
-
-            # 마지막 비디오 번호를 파일에 저장
-            with open(last_index_file, 'w') as f:
-                f.write(str(last_num))
-
-        return df
-
-
-def main():
-    df = finding_video()
-
-    SCOPES = ['https://www.googleapis.com/auth/calendar']
-
-    CREDENTIALS_FILE = 'client_secret.json'
-
-    creds = None
-    # token.pickle은 사용자의 접근과 토큰을 저장합니다.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                CREDENTIALS_FILE, SCOPES)
-            creds = flow.run_local_server(port=5090)
-
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('calendar', 'v3', credentials=creds)
-
-    for r in range(len(df)):
-        title = '[과목이름] ' + df['title'][r]
-        start_date = pd.to_datetime(df['date'][r].split(' ~ ')[0], format='%Y/%m/%d %H:%M').isoformat()
-        end_date = pd.to_datetime(df['date'][r].split(' ~ ')[-1], format='%Y/%m/%d %H:%M').isoformat()
-
-        service.events().insert(calendarId='primary',
-                                               body={
-                                                   'summary': title,
-                                                   'start': {'dateTime': start_date, 'timeZone': 'Asia/Seoul'},
-                                                   'end': {'dateTime': end_date, 'timeZone': 'Asia/Seoul'},
-                                               }
-                                               ).execute()
-
-
-if __name__ == '__main__':
-    main()
+print("created event")
+print("id: ", event_result['id'])
+print("summary: ", event_result['summary'])
+print("starts at: ", event_result['start']['dateTime'])
+print("ends at: ", event_result['end']['dateTime'])
